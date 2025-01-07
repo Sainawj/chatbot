95afcf9adfc8603ad84a1310a3ba5a8b32d01cc7
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2025-01-07 18:53:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 MONGO_URI = 'mongodb://localhost:27017/chatbot'
 
 # Flask Secret Key for sessions
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = 'f7a5ea38aa64fc9ea8251e9c904729f0ff13731fa2f4a5c1ac1cc142353aa1f6'
